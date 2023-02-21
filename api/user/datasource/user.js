2ee6214eefe5603853f24e64b2f0e1e02dfd429c
marcossnikel <marcosnikeldev@gmail.com>
2023-02-20 23:29:53 -0300

    :books: feat: adding datasources methods

---
+++ api/user/datasource/user.js
@@ -0,0 +1,44 @@
+const { RESTDataSource } = require('apollo-datasource-rest')
+//coleção de calsses
+
+
+class UsersAPI extends RESTDataSource{
+    constructor(){
+        super()
+        this.baseURL = 'http://localhost:3000'
+    }
+
+    async getUsers(){
+        const users = await this.get('/users')
+        return users.map(async user =>({
+            id:  user.id,
+            nome: user.nome,
+            email: user.email,
+            ativo: user.ativo,
+            role: await this.get(`/roles/${user.role}`)
+        }))
+    }
+
+    async getUserById(id){
+        const user = await this.get(`/users/${id}`)
+        user.role = await this.get(`/roles/${user.role}`)
+        return user
+    }
+
+    async createUser(user){
+        const users = await this.get('/users')
+        user.id = users.length + 1
+        const role = await this.get(`roles?type=${user.role}`)
+
+        await this.post('users',{...users,role: role[0].id})
+
+        return ({
+            ...user,
+            role: role[0]
+        })
+    }
+}
+
+
+
+module.exports = UsersAPI
